35a29374768ca97efac94dfbc1bb3963a52686ad
G A ARJUN <gaarjun00@gmail.com>
2026-03-26 10:55:23 +0530

    routes management

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
-const http = require('http');
-// The developer is trying to import the logMessage function
-// using destructuring, assuming an object is returned.
-const { logMessage } = require('./utils/log');
+const express = require('express')
+const app = express()
+const PORT = 3000
 
-const server = http.createServer((req, res) => {
-  // This line will crash the server because logMessage is undefined.
-  logMessage('A new request was received!'); 
+const postRouter = require("./routes/posts.routes")
 
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!');
-});
+app.get("/", async (req,res) => {
+  res.send("Welcome to the Blogify API! This is the main entry point.")
+})
 
-const PORT = 3000;
+app.use("/api/v1/posts", postRouter)
 
-server.listen(PORT, () => {
-  console.log(`Server is attempting to run at http://localhost:${PORT}/`);
-});
+app.listen(PORT, () => {
+  console.log(`Server running successfully on port ${PORT}`)
+})
