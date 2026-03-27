007ad39d7ee2e50ed0679cd5256b048b34a7428e
G A ARJUN <gaarjun00@gmail.com>
2026-03-27 06:41:07 +0530

    refactor: standardize JSON responses and status codes

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -1,11 +1,14 @@
 
 const getAllPosts = (req,res) => {
-    res.send("Fetching all blog posts from the modular router!")
+    res.status(200).json({
+        "succes": true,
+        "data": "Fetching all blog posts from the modular router!"
+    })
 }
 
 const getPostsById = async (req,res) => {
     const postId = await req.params.id
-    res.json({ "message": "Fetching data for post with ID: " + postId })
+    res.status(200).json({ "success": true, "data": {"Fetching data for post with ID: " : postId}})
 }
 
 module.exports = {getAllPosts, getPostsById};
