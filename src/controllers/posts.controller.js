b744dbae25004e6bff044b40330595177866f06b
G A ARJUN <gaarjun00@gmail.com>
2026-03-27 06:23:28 +0530

    feat: add endpoint to get post by id

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -3,4 +3,9 @@ const getAllPosts = (req,res) => {
     res.send("Fetching all blog posts from the modular router!")
 }
 
-module.exports = getAllPosts;
+const getPostsById = async (req,res) => {
+    const postId = await req.params.id
+    res.json({ "message": "Fetching data for post with ID: " + postId })
+}
+
+module.exports = {getAllPosts, getPostsById};
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const router = express.Router()
 
-const postController = require("../controllers/posts.controller")
+const {getAllPosts, getPostsById} = require("../controllers/posts.controller")
 
-router.get("/", postController)
+router.get("/", getAllPosts)
+
+router.get("/:id", getPostsById)
 
 router.post("/", (req,res) => {
     res.send('Creating a new blog post...')
