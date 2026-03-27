c8eb843b9a7be9c7808460fccc126bbd97fd1ebd
G A ARJUN <gaarjun00@gmail.com>
2026-03-27 06:01:18 +0530

    refactor: move post logic to controller

---
+++ src/controllers/posts.controller.js
@@ -0,0 +1,6 @@
+
+const getAllPosts = (req,res) => {
+    res.send("Fetching all blog posts from the modular router!")
+}
+
+module.exports = getAllPosts;
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const router = express.Router()
 
-router.get("/", (req,res) => {
-    res.send("Fetching all blog posts from the modular router!")
-})
+const postController = require("../controllers/posts.controller")
+
+router.get("/", postController)
 
 router.post("/", (req,res) => {
     res.send('Creating a new blog post...')
